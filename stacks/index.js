e5fb05526fdb62ab1104bb2944857a72b738b62f
Pat <patgalvin25@icloud.com>
2021-10-26 22:41:37 -0400

    switch commit

--- stacks/index.js
+++ stacks/index.js
@@ -1,17 +1,6 @@
-// import MyStack from "./MyStack";
-
-// export default function main(app) {
-//   // Set default runtime for all functions
-//   app.setDefaultFunctionProps({
-//     runtime: "nodejs12.x"
-//   });
-
-//   new MyStack(app, "my-stack");
-
-//   // Add more stacks
-// }
-import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
+import StorageStack from "./StorageStack";
+
 
 
 export default function main(app) {
